ad6f37b0a134c49dc1f43b652cfef5341765fcb2
Guillaume Grasset <guillaume.grasset@piiou.net>
2016-06-03 22:32:40 -0400

    usable version

--- checkItForMe.js
+++ checkItForMe.js
@@ -4,14 +4,18 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://scrap.tf/raffles
+// @match        SOME-URL
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var RELOAD_DELAY = 60;
+    var RELOAD_DELAY = 60,
+        ENTERING_DELAY = 2;
+
+    var todoRaffleList = [],
+        raffleIndex = 0;
 
     $(document).ready(function() {
         scanHash();
@@ -20,69 +24,78 @@
     function scanHash() {
         var hash = window.location.hash,
             url = window.location.href,
-            u = "https://scrap.tf/raffles";
+            baseUrl = /SOME-URL[\/]*/;
 
-        if (url === u) {
-            console.log("scanning the raffles...");
+        if (url.match(baseUrl)) {
+
             scanRaffles();
-
-            // joinRaffle();
 
             setTimeout(function() {
                 location.reload();
             }, RELOAD_DELAY * 1000);
         }
 
-        // if (url.indexOf(u) > -1 && url != u) {
-        //     if (hash === "#join") {
-        //         console.log("Joining raffle...");
-        //     }
-        //     else {
-        //         window.close();
-        //     }
-        // }
     }
 
     function scanRaffles() {
-
         $.when(scrollToBottom()).then(function() {
-            $('div.panel-raffle').each(function(item) {
-                // DEBUG:
-                console.log(item);
+            $('div.panel-raffle').each(function(id, item) {
+                if($(item).css('opacity') === '1'){
+                    todoRaffleList.push($(item).find('div.raffle-name > a').attr('href'));
+                }
             });
+            if(todoRaffleList.length >0){
+                enterRaffles(todoRaffleList);
+            }
         });
-
-        // $(".panel").each(function() {
-        //     pc++;
-        //     $(this).attr("id", "raffle-panel-" + pc);
-        // });
-        //
-        // if (pc > 1) {
-        //     $("#raffle-panel-1").hide();
-        //     cId = "raffle-panel-2";
-        // }
-
-        // $(cId).find(".panel-raffle").each(function() {
-        //     var o = $(this).css("opacity");
-        //     if (o == "0.6") {
-        //         $(this).hide();
-        //     }
-        //     else {
-        //         var r = $(this).attr("id"),
-        //             raffleId = r.replace("raffle-box-", "");
-        //
-        //         if (openedTabs < 5) {
-        //             var u = window.location.href + "/" + raffleId + "#join";
-        //             var win = window.open(u, "", "width=0,height=0");
-        //             win.blur();
-        //             win.resizeTo(0, 0);
-        //             win.moveTo(0, window.screen.availHeight + 10);
-        //             openedTabs++;
-        //             $(this).css("opacity", "0.6").hide();
-        //         }
-        //     }
-        // });
     }
+
+    function enterRaffles(raffleList){
+        console.log('enter raffles:',raffleList);
+
+        function joinRaffle(url){
+            var currentChildWindow = window.open(url);
+
+            $(currentChildWindow.document).ready(function(){
+                var waitInterval;
+                console.log('entrering raffle:',url);
+
+                waitInterval= setInterval(function() {
+                    console.log('waiting confirmation…');
+
+                    if($(currentChildWindow.document).find('button#raffle-enter>i18n').html() === 'Enter Raffle'){
+
+                        $(currentChildWindow.document).find('button#raffle-enter').trigger( 'click' );
+
+                    }else if($(currentChildWindow.document).find('button#raffle-enter>i18n').html() === 'Leave Raffle' || $(currentChildWindow.document).find('div.alert-error').css('display') !== 'none'){
+
+                        console.log('closing window');
+
+                        currentChildWindow.close();
+
+                        raffleIndex++;
+                        if(raffleIndex < todoRaffleList.length){
+                            setTimeout(function() {
+                                joinRaffle(todoRaffleList[raffleIndex]);
+                            }, ENTERING_DELAY*1000);
+                        }
+
+                        clearInterval(waitInterval);
+
+                    }
+                },1000);
+
+
+
+            });
+
+
+        }
+
+        joinRaffle(todoRaffleList[raffleIndex]);
+    }
+
+
 
     function scrollToBottom() {
         var deferred = jQuery.Deferred(),
@@ -91,7 +104,7 @@
         var scrollInterval = setInterval(function() {
 
             if (keepScrolling) {
-                $('html, body').animate({scrollTop: $(document).height()}, 500);
+                $('html, body').animate({scrollTop: $(document).height()}, 50);
             } else {
                 deferred.resolve();
                 clearInterval(scrollInterval);
@@ -99,20 +112,9 @@
 
             keepScrolling = !($('.pag-loading').text() === 'That\'s all, no more!');
 
-        }, 1000);
+        }, 100);
 
         return deferred.promise();
 
-    }
-
-    function joinRaffle() {
-        window.opener.focus();
-        var onclick = $("#raffle-enter").attr('onclick');
-        eval("var sc = function(){" + onclick + "}");
-        sc();
-        window.location.hash = "#";
-        setTimeout(function() {
-            window.close();
-        }, 10000);	// close the page after 10 sec
     }
 })();
