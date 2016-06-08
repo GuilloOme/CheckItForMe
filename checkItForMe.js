b5f161a5040683f4d877e539011113a7d6b8faa3
Guillaume Grasset <guillaume@delvelabs.ca>
2016-06-08 10:39:05 -0400

    update version number

--- checkItForMe.js
+++ checkItForMe.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CheckItForMe
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @match        https://scrap.tf/raffles
 // @require      https://code.jquery.com/jquery-2.2.4.min.js#sha256=BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=
 // @grant        none
