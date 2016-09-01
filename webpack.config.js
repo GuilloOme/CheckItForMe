aaa5ed8834f2172966e8ba26820ea5427acb61bc
Guillaume Grasset <guillaume@delvelabs.ca>
2016-09-01 16:05:52 -0400

    update the updateURL

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@
                         '// @version      0.49',
                         '// @match        https://scrap.tf/raffles',
                         '// @require      https://code.jquery.com/jquery-2.2.4.min.js#sha256=BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44',
-                        '// @updateURL    https://raw.githubusercontent.com/GuilloOme/CheckThisForMe/master/checkItForMe.js',
+                        '// @updateURL    https://raw.githubusercontent.com/GuilloOme/CheckItForMe/master/dist/check-it-for-me.js',
                         '// @grant        none',
                         '// ==/UserScript=='
                     ].join('\n'),
