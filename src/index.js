b97c1d2d3c489b7b9f292a1b1cdd9f3eadf6b7da
Guillaume Grasset <guillaume@delvelabs.ca>
2016-09-02 12:18:20 -0400

    create a raffle object use a storage service

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 (function () {
     'use strict';
 
-    var Raffle = require('./raffle.service').RaffleService,
+    var RafflesService = require('./raffles.service').RafflesService,
         Interval = require('./interval.helper').IntervalHelper,
         UI = require('./ui.helper').UIHelper;
 
-    var RELOAD_DELAY = 50,
-        ERROR_RELOAD_DELAY = 300,
-        ENTERING_DELAY = 3;
+    var RELOAD_DELAY = 50;
 
-    var todoRaffleList = Raffle.goodList,
-        badRaffleList = Raffle.badList,
-        raffleIndex = 0,
-        interval = Interval.randomInterval(RELOAD_DELAY),
-        haveStorageSupport = false;
+    var interval = Interval.randomInterval(RELOAD_DELAY);
 
 
     $(document).ready(function () {
@@ -22,21 +16,6 @@
 
         UI.showMessage('Started');
 
-        if (typeof(Storage) !== "undefined") {
-            haveStorageSupport = true;
-
-            // initiate the locals
-            if (typeof localStorage.badRaffleList !== 'string') {
-                saveBadRaffleList([]);
-            }
-
-            if (JSON.parse(localStorage.badRaffleList).length > 1000) {
-                console.warn('Purging bad raffle cache!');
-                saveBadRaffleList([]);
-            } else {
-                badRaffleList = badRaffleList.concat(JSON.parse(localStorage.badRaffleList));
-            }
-        }
 
         if (UI.getUserNoticeCount() > 0) {
             UI.showMessage('There is new message(s)!');
@@ -46,31 +25,34 @@
 
         UI.showMessage('Loading all the raffles…');
 
-        $.when(loadAllRaffles()).then(function () {
+        $.when(loadAllRafflesInPage()).then(function () {
             var activePanel = $('div.panel');
             $(activePanel[activePanel.length - 1]).find('div.panel-raffle').each(function (id, item) {
                 var url = $(item).find('div.raffle-name > a').attr('href');
 
-                // DEBUG: check everything
-                // if ($(item).css('opacity') === '1' && badRaffleList.indexOf(url) < 0) {
-                    todoRaffleList.push(url);
-                // }
+                if ($(item).css('opacity') === '1' && RafflesService.badList.indexOf(url) < 0) {
+                    RafflesService.goodList.push(url);
+                }
             });
 
-            if (todoRaffleList.length > 0) {
-                //UI.showMessage('Start entering raffles.');
+            if (RafflesService.goodList.length > 0) {
+                //Start entering raffles
 
-                $.when(enterRaffles()).then(function () {
-                    //UI.showMessage('Done entering raffles, reloading…');
-                    // DEBUG:
-                    console.log('no reload');
-                    //location.reload();
+                UI.addProgress('', 0, 'success');
+
+                $.when(RafflesService.enterRaffles()).then(function () {
+                    //Done entering raffles, reloading…
+                    location.reload();
                 });
             } else {
+
+                var interval = Interval.randomInterval(RELOAD_DELAY),
+                    timer = 1000;
+
                 UI.showMessage('No new raffle to enter, waiting…');
 
                 UI.addProgress('', 0);
-                var timer = 1000;
+
                 setInterval(function () {
                     UI.updateProgress(timer / interval);
                     timer += 250;
@@ -84,101 +66,7 @@
 
     });
 
-    function enterRaffles() {
-        var deferred = jQuery.Deferred();
-
-        UI.addProgress('', 0, 'success');
-
-        function joinRaffle(url) {
-
-            var id, hash;
-
-            ScrapTF.Raffles.EnterRaffle = function (idArg, hashArg) {
-                id = idArg;
-                hash = hashArg;
-            };
-
-            UI.showMessage('Checking raffle: ' + (raffleIndex + 1) + '/' + todoRaffleList.length);
-
-            $.get(url, function (responseData) {
-
-                var request,
-                    raffleDeferred = jQuery.Deferred(),
-                    raffleKey = $(responseData).find("#raffle-key").val(),
-                    enterButton = $(responseData).find('button#raffle-enter');
-
-                if (Raffle.isRaffleWorthIt(responseData)) {
-                    // DEBUG: Do not enter!!
-                    if (enterButton.length > 0 && $(responseData).find('button#raffle-enter>i18n').html() === 'Enter Raffle' && false) {
-                        UI.showMessage('Entering raffle: ' + (raffleIndex + 1) + '/' + todoRaffleList.length);
-
-                        $(responseData).find('button#raffle-enter').click();
-
-                        request = {
-                            raffle: id,
-                            captcha: '',
-                            rafflekey: raffleKey,
-                            password: '',
-                            hash: hash
-                        };
-
-                        ScrapTF.Ajax('viewraffle/EnterRaffle', request, function () {
-                            raffleDeferred.resolve('Done entering raffle: ' + (raffleIndex + 1) + '/' + todoRaffleList.length, true);
-                        }, function (data) {
-
-                            if (data.captcha) {
-                                showIcon('Warning');
-
-                                setTimeout(function () {
-                                    location.reload();
-                                }, ERROR_RELOAD_DELAY * 1000);
-
-                                raffleDeferred.reject('Captcha requested! Reloading in ' + ERROR_RELOAD_DELAY / 60 + 'minutes…');
-                            } else {
-                                raffleDeferred.resolve('Error when entering raffle: ' + (raffleIndex + 1) + '/' + todoRaffleList.length + ' - ' + data.message);
-                            }
-                        });
-
-                    } else {
-                        raffleDeferred.resolve('Can\'t enter raffle: ' + (raffleIndex + 1) + '/' + todoRaffleList.length + ' (no button to click)');
-                    }
-                } else {
-                    badRaffleList.push(url);
-                    saveBadRaffleList(badRaffleList);
-                    raffleDeferred.resolve('Raffle not worth it: ' + (raffleIndex + 1) + '/' + todoRaffleList.length);
-                }
-
-                $.when(raffleDeferred.promise()).then(function (message, haveToWait) {
-                    var interval = Interval.randomInterval();
-
-                    UI.showMessage(message);
-
-                    UI.updateProgress((raffleIndex + 1) / todoRaffleList.length, (raffleIndex + 1) + '/' + todoRaffleList.length);
-
-                    if (haveToWait) {
-                        interval = Interval.randomInterval(ENTERING_DELAY);
-                    }
-
-                    raffleIndex++;
-                    if (raffleIndex < todoRaffleList.length) {
-                        setTimeout(function () {
-                            joinRaffle(todoRaffleList[raffleIndex]);
-                        }, interval);
-                    } else {
-                        deferred.resolve();
-                    }
-                });
-
-            });
-
-        }
-
-        joinRaffle(todoRaffleList[raffleIndex]);
-
-        return deferred.promise();
-    }
-
-    function loadAllRaffles() {
+    function loadAllRafflesInPage() {
         var deferred = jQuery.Deferred();
 
         var loadInterval = setInterval(function () {
@@ -194,12 +82,6 @@
 
         return deferred.promise();
 
-    }
-
-    function saveBadRaffleList(list) {
-        if (haveStorageSupport) {
-            localStorage.badRaffleList = JSON.stringify(list);
-        }
     }
 
 })();
